8db5d92a69fe7fd746c422ba6f9610e78ffb6075
nori <a@a.a>
2021-05-08 12:17:13 +0200

    tmp

--- tests/strlen/tmp.c
+++ tests/strlen/tmp.c
@@ -14,7 +14,15 @@ int f_signal()
 	return (0);
 }
 
-int f(int (*func)())
+int f_ret()
+{
+	char *s = "aiueo";
+	(void)s;
+	// s[0] = 1;
+	return (0);
+}
+
+int fork_test(int (*func)())
 {
 	pid_t	pid;
 	int		status;
@@ -29,21 +37,25 @@ int f(int (*func)())
 	//00    00    00     00
 	//            ↑      ↑
 	//          返り値  SIGNAL
-	if (WIFEXITED(status))
+	if (WIFEXITED(status))	//return 0 or -1
 		return ((char)WEXITSTATUS(status));
-	if (WIFSIGNALED(status))
+	if (WIFSIGNALED(status)) //異常終了 segv buse abort etc...
 		return (WTERMSIG(status));
 	return (0);
 }
 
-int main()
+void res(int ret)
 {
-	int ret = f(f_signal);
-
 	if (!ret)
 		printf("OK\n");
 	else if (ret == SIGSEGV)
 		printf("SEGV\n");
 	else if (ret == SIGBUS)
 		printf("BUS\n");
+}
+
+int main()
+{
+	res(fork_test(f_ret));
+	res(fork_test(f_signal));
 }
