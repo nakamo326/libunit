f45c1bae5dbafda6f6c8165e6ccaaa2c6eebd8cc
nori <a@a.a>
2021-05-08 20:24:01 +0200

    fix

--- tests/strlen/00_launcher.c
+++ tests/strlen/00_launcher.c
@@ -3,5 +3,15 @@
 
 int	strlen_launcher(void)
 {
-	
+	t_clist *lst;
+
+	puts("\x1b[1mSTRLEN\x1b[0m\n");
+	load_test(&lst, "basic_test", basic_test);
+	// load_test(&lst, "", );
+	// load_test(&lst, "", );
+	// load_test(&lst, "", );
+	// load_test(&lst, "", );
+	// load_test(&lst, "", );
+	// load_test(&lst, "", );
+	return (launch_tests(&lst));
 }
