acf79dd283370d4ef0ac1dc8918f6d68cc8a1c46
nori <a@a.a>
2021-05-09 19:54:35 +0200

    a

--- tests/strlen/00_launcher.c
+++ tests/strlen/00_launcher.c
@@ -20,9 +20,9 @@ int	strlen_launcher(void)
 	load_test(&lst, "include_nullchar_test", include_nullchar_test);
 	// load_test(&lst, "bigger_str_test", bigger_str_test);
 	load_test(&lst, "include_allascci_test", include_allascii_test);
-	// load_test(&lst, "NULL_test", null_test);
-	// load_test(&lst, "bus_error", bus_error);
-	// load_test(&lst, "ko_case", ko_case);
-	// load_test(&lst, "zero_divide", zero_divide);
+	load_test(&lst, "NULL_test", null_test);
+	load_test(&lst, "bus_error", bus_error);
+	load_test(&lst, "ko_case", ko_case);
+	load_test(&lst, "zero_divide", zero_divide);
 	return (launch_tests(&lst));
 }
