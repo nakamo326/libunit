7f8b240afc7c8fd3a5a033de8e43e679dc82b027
nori <a@a.a>
2021-05-09 16:44:35 +0200

    init launcher

---
+++ tests/itoa/00_launcher.c
@@ -0,0 +1,23 @@
+#include "../../framework/libunit.h"
+
+int	itoa_launcher(void)
+{
+	t_clist *lst;
+
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	load_test(&lst, "", );
+	return (launch_tests(&lst));
+}
