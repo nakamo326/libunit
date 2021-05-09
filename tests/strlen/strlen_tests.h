9cc96ccd57f577f7b41e8d0768ab067023007337
nori <a@a.a>
2021-05-09 20:04:06 +0200

    a

--- tests/strlen/strlen_tests.h
+++ tests/strlen/strlen_tests.h
@@ -13,7 +13,7 @@ int	basic_test5(void);
 int	basic_test6(void);
 int	basic_test7(void);
 int	basic_test8(void);
-int include_allascii_test(void);
+int	include_allascii_test(void);
 int	not_endwith_nullchar(void);
 int	include_space_test(void);
 int	empty_str_test(void);
