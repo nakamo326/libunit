21b4d4c166fbee9f766930483b703e5ea5ac7039
nori <a@a.a>
2021-05-12 08:23:27 +0200

    a

--- real-tests/init.sh
+++ real-tests/init.sh
@@ -25,45 +25,6 @@ LIBUNIT_DIR		= ../
 SRCS			= $(find . -name '*.c' | tr '\n' ' ')
 "
 
-
-makefile+='
-OBJS			= $(SRCS:.c=.o)
-
-all	:	$(NAME)
-
-$(NAME) : $(OBJS)
-	$(MAKE) -C $(LIBFT_DIR)
-	$(MAKE) -C $(FRAMEWORK_DIR)
-	cp $(LIBFT_DIR)/libft.a $(LIBUNIT_DIR)
-	cp $(FRAMEWORK_DIR)/libunit.a $(LIBUNIT_DIR)
-	$(RM) ../result.txt
-
-test		: $(NAME)
-	$(RM) ../result.txt
-	$(CC) $(OBJS) $(LIBUNIT_DIR)/libft.a $(LIBUNIT_DIR)/libunit.a $(CFLAGS) -o $(NAME) && ./libunit
-
-add	:
-	bash init.sh
-
-clean:
-	rm -f $(OBJS)
-
-bonus : $(OBJS)
-	$(MAKE) -C $(LIBFT_DIR)
-	$(MAKE) bonus -C $(FRAMEWORK_DIR)
-	cp $(LIBFT_DIR)/libft.a $(LIBUNIT_DIR)
-	cp $(FRAMEWORK_DIR)/libunit.a $(LIBUNIT_DIR)
-	$(RM) ../result.txt
-
-fclean: clean
-	$(MAKE) fclean -C $(LIBFT_DIR)
-	$(MAKE) fclean -C $(FRAMEWORK_DIR)
-	rm -f $(NAME) $(LIBUNIT_DIR)/libft.a $(LIBUNIT_DIR)/libunit.a
-
-re:		fclean all
-
-.PHONY:	all clean fclean re bonus'
-
 MAIN_C='#include "cases.h"
 #include <stdio.h>
 
@@ -83,4 +44,6 @@ MAIN_C+="$(find . -name '*.c' | sed '/\/00.*\.c/!d' | xargs cat 2>/dev/null | se
 
 echo "${MAIN_C}" > main.c
 echo "${HEADER}" > cases.h
-echo "${makefile}" > Makefile
+
+files=$(find . -name '*.c' | tr '\n' ' ')
+sed -i -e "s@^SRCS\s*=.*@SRCS\t\t\t\t= ${files}@g" Makefile
--- tests/init.sh
+++ tests/init.sh
@@ -14,54 +14,8 @@ HEADER+="# define ${NAME}${INCLUDE}"
 HEADER+=$(find . -type d -name libft -prune -o -name '*.c'  | xargs cat 2>/dev/null | sed -e '/^\w/!d' | sed -e "s/)$/);/g")
 HEADER+="${LF}${LF}#endif"
 
-
-makefile="NAME			= libunit
-CC				= gcc
-CFLAGS			= -Wall -Wextra -Werror
-LIBFT_DIR		= ../real-tests/libft
-FRAMEWORK_DIR	= ../framework
-LIBUNIT_DIR		= ../
-SRCS			= $(find . -name '*.c' | tr '\n' ' ')
-"
-
-
-makefile+='
-OBJS			= $(SRCS:.c=.o)
-
-all	:	$(NAME)
-
-$(NAME) : $(OBJS)
-	$(MAKE) -C $(LIBFT_DIR)
-	$(MAKE) -C $(FRAMEWORK_DIR)
-	cp $(LIBFT_DIR)/libft.a $(LIBUNIT_DIR)
-	cp $(FRAMEWORK_DIR)/libunit.a $(LIBUNIT_DIR)
-	$(RM) ../result.txt
-
-test		: $(NAME)
-	$(RM) ../result.txt
-	$(CC) $(OBJS) $(LIBUNIT_DIR)/libft.a $(LIBUNIT_DIR)/libunit.a $(CFLAGS) -o $(NAME) && ./libunit
-
-add	:
-	bash init.sh
-
-clean:
-	rm -f $(OBJS)
-
-bonus : $(OBJS)
-	$(MAKE) -C $(LIBFT_DIR)
-	$(MAKE) bonus -C $(FRAMEWORK_DIR)
-	cp $(LIBFT_DIR)/libft.a $(LIBUNIT_DIR)
-	cp $(FRAMEWORK_DIR)/libunit.a $(LIBUNIT_DIR)
-	$(RM) ../result.txt
-
-fclean: clean
-	$(MAKE) fclean -C $(LIBFT_DIR)
-	$(MAKE) fclean -C $(FRAMEWORK_DIR)
-	rm -f $(NAME) $(LIBUNIT_DIR)/libft.a $(LIBUNIT_DIR)/libunit.a
-
-re:		fclean all
-
-.PHONY:	all clean fclean re bonus'
+files=$(find . -name '*.c' | tr '\n' ' ')
+sed -i -e "s@^SRCS\s*=.*@SRCS\t\t\t\t= ${files}@g" Makefile
 
 MAIN_C='#include "cases.h"
 #include <stdio.h>
@@ -82,4 +36,3 @@ MAIN_C+="$(find . -name '*.c' | sed '/\/00.*\.c/!d' | xargs cat 2>/dev/null | se
 
 echo "${MAIN_C}" > main.c
 echo "${HEADER}" > cases.h
-echo "${makefile}" > Makefile
