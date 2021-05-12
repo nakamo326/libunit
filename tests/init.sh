a24d23e25ae00468a28295d6890464012cfd5b59
nori <a@a.a>
2021-05-12 14:35:20 +0200

    a

--- tests/init.sh
+++ tests/init.sh
@@ -14,11 +14,11 @@ int	main(void)
 	ret = 0;
 '
 
-MAIN_C+="$(find . -type f -name '*.c' | sed '/\/00.*\.c/!d' | xargs cat | sed -e '/^[a-zA-Z]/!d' -e "s/(.*)$/();/g" -e "s/^[a-zA-Z][a-zA-Z_0-9]*\t/\tret += /g")
+MAIN_C+="$(find . -type f -name '*.c' | sed '/\/00.*\.c/!d' | xargs cat | sed -e '/^[a-zA-Z]/!d' -e "s/(.*)$/();/g" -e "s/^[a-zA-Z][a-zA-Z_0-9]*$(printf "\t")/$(printf "\t")ret += /g")
 	return (ret);
 }"
 
-makefile=$(sed -e "s@^SRCS\s*=.*@SRCS\t\t\t= $(find . -type f -name '*.c' | tr '\n' ' ' | sed 's/\s*$//')@g" Makefile)
+makefile=$(sed -e "s@^SRCS\s*=.*@SRCS$(printf "\t")$(printf "\t")$(printf "\t")= $(find . -type f -name '*.c' | tr '\n' ' ' | sed 's/\s*$//')@g" Makefile)
 header="$(sed -e "/^[a-zA-Z][a-zA-Z_0-9]*.*);$/d" -e '/#endif/d' -e '/^$/d' ${NAME_H})
 
 $(find . -type d -name libft -prune -o -type f -name '*.c' | xargs cat | sed -e '/^[a-zA-Z].*)$/!d' -e '/^static/d' -e "s/)$/);/g")
