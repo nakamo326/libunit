087029be91b38a9e13289757359a1a8558056720
nori <a@a.a>
2021-05-13 19:00:28 +0200

    a

--- framework/header.sh
+++ framework/header.sh
@@ -1,6 +1,12 @@
-prot=$(find . -name '*.c' | xargs cat | sed -e '/^\w/!d')
-# echo "${prot}"
+NAME_H="libunit.h"
+
+prot=$(find . -type d -name libft -prune -o -type f -name '*.c' | xargs cat | sed -e '/^[a-zA-Z].*)$/!d' -e '/^static/d' -e "s/)$/);/g")
 TABS=$(echo "${prot}" | sed 's/\t.*//g' | awk '{print length($0)}' | sort -nr | head -n 1 | xargs -I{} expr {} / 4 + 1)
 
-echo "${prot}" | sed 's/\t//g'
-# echo "${prot}" | awk '{print $0}' | sed 's/\t//g'
+header="$(sed -e "/^[a-zA-Z][a-zA-Z_0-9]*.*);$/d" -e '/#endif/d' -e '/^$/d' ${NAME_H})
+
+$(echo "${prot}" | awk -v tabs=${TABS} '{s=$0; sub("\t.*", ""); l=tabs - int(length($0) / 4); t="";for(i=0;i<l;i++){t=t "\t"};sub("\t", t, s); print s}')
+
+#endif"
+
+echo "${header}" > libunit.h
