2c48bf76ee44e78550339109e3a0e7c87a8a6086
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-03-05 17:47:39 +0900

    ファイル分割

--- framework/includes/libunit.h
+++ framework/includes/libunit.h
@@ -1,6 +1,7 @@
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
 # include <stdlib.h>
+# include <stdbool.h>
 # include <stdint.h>
 # include <string.h>
 # include <sys/types.h>
@@ -43,27 +44,28 @@ typedef struct s_case
 	int		(*f_case)();
 }	t_case;
 
-int		get_result();
+bool	exists_pid(t_clist *found);
+int		get_result(void);
 int		launch_tests(t_clist **lst, char *title);
 int		print_result_one(char *title, char *case_name, int res, size_t max_len);
 size_t	get_max_len_of_case_name(t_clist *suite);
-size_t	get_success_count();
-size_t	get_testcount();
+size_t	get_success_count(void);
+size_t	get_testcount(void);
 t_clist	*find_pid_from_finished(pid_t target, t_clist *waited);
 void	err_exit(t_clist *lst, t_case *data);
-void	inc_success_count();
-void	inc_testcount();
+void	inc_success_count(void);
+void	inc_testcount(void);
 void	init_test(t_clist **lst);
 void	load_test(t_clist **lst, char *case_name, int (*f_case)());
 void	print_cases_result(t_clist *suite, char *title);
-void	print_suite_result();
-void	reset_success_count();
-void	reset_testcount();
+void	print_result_all(t_clist *suite, char *title);
+void	print_suite_result(void);
+void	reset_success_count(void);
+void	reset_testcount(void);
+void	run_suite(t_clist *suite, char *title);
 void	run_test(t_clist *lst);
 void	set_success_count(size_t value);
 void	set_testcount(size_t value);
 void	wait_case(t_clist *pidlst);
-void	print_result_all(t_clist *suite, char *title);
-void	run_suite(t_clist *suite, char *title);
 
 #endif
---
+++ framework/srcs/framework/print_result_all.c
@@ -0,0 +1,48 @@
+#include "libunit.h"
+#include "ft_list.h"
+#include "libft.h"
+
+void	print_cases_result(t_clist *suite, char *title)
+{
+	size_t	max_len;
+	t_proc	*proc;
+	t_case	*testcase;
+	t_clist	*found;
+	t_clist	*finished;
+
+	max_len = get_max_len_of_case_name(suite);
+	finished = ft_clstnew(NULL);
+	suite = ft_clstfirst(suite);
+	while (!ft_clst_isend(suite))
+	{
+		testcase = suite->data;
+		found = find_pid_from_finished(testcase->pid, finished);
+		if (exists_pid(found))
+		{
+			proc = found->data;
+			print_result_one(title, testcase->case_name, proc->res, max_len);
+			if (proc->res == 0)
+				inc_success_count();
+			ft_clst_popdel(found, free);
+			suite = suite->next;
+		}
+		wait_case(finished);
+	}
+	ft_clst_clear(&finished, free);
+}
+
+void	print_suite_result(void)
+{
+	ft_putstr_fd("\n", STDOUT_FILENO);
+	ft_putnbr_fd(get_success_count(), STDOUT_FILENO);
+	ft_putstr_fd("/", STDOUT_FILENO);
+	ft_putnbr_fd(get_testcount(), STDOUT_FILENO);
+	ft_putendl_fd(" tests passed\n", STDOUT_FILENO);
+	ft_putendl_fd(CYAN BOLD BORDER "\n" B_RESET RESET, STDOUT_FILENO);
+}
+
+void	print_result_all(t_clist *suite, char *title)
+{
+	print_cases_result(suite, title);
+	print_suite_result();
+}
